f41c01c35b74c60dd1cf9469d16ebf41e56e9347
Epo123 <erik.poolman123@gmail.com>
2014-03-03 15:01:26 +0100

    winkelwagen wordt verstuurd naar afrekenen pagina

--- kroket/views.py
+++ kroket/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.template import RequestContext, loader, Library
 
 def index(request):
     template = loader.get_template('index.html')
@@ -18,9 +18,32 @@ def assortiment(request):
     return HttpResponse(template.render(context))
 
 def afrekenen(request):
+    if request.method == 'POST':
+
+        cart = []
+
+        article_names = request.POST.getlist('artname[]')
+        article_quantities = request.POST.getlist('artquant[]')
+
+        for index, article_name in enumerate(article_names):
+            cart.append({
+                'name': article_name,
+                'quantity': article_quantities[index],
+                'price': round(float(article_quantities[index]) * 9.9, 2)
+            })
+
+        test = request.POST.getlist('artname[]')
+        artquant = request.POST.getlist('artquant[]')
+    else:
+        test = []
+        artquant = []
+        cart = []
+
     template = loader.get_template('afrekenen.html')
 
     context = RequestContext(request, {
-
+        'test': test,
+        'artquant': artquant,
+        'cart': cart,
     })
     return HttpResponse(template.render(context))
